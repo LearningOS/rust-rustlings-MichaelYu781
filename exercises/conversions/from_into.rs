13c803955d2c33b4001d5a62158b5c5b50ba6e33
MichaelYu <michaelyu781@126.com>
2023-04-25 21:39:46 +0800

    Todo

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,42 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let s = s.to_string();
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let mut iter = s.split(",");
+        let name: String;
+        let age: usize;
+
+        if let Some(na) = iter.next() {
+            name = na.to_string();
+            if name.len() == 0 {
+                return Person::default();
+            }
+
+        } else {
+            return Person::default();
+        }
+
+        if let Some(a) = iter.next() {
+            if let Ok(x) = a.parse::<usize>() {
+                age = x;
+            } else {
+                return Person::default();
+            }
+        } else {
+            return Person::default();
+        }
+
+        if let Some(_a) = iter.next() {
+            return Person::default();
+        }
+
+        Person{name, age}
     }
 }
 
